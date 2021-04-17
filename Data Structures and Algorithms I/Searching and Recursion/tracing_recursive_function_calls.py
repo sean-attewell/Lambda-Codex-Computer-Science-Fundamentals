a009a46d65aa024c452a49e76598fd1cf87cf838
sean-attewell <sean.attewell@gmail.com>
2021-04-17 13:20:37 +0100

    tracing recursion and binary search

---
+++ Data Structures and Algorithms I/Searching and Recursion/tracing_recursive_function_calls.py
@@ -0,0 +1,79 @@
+# What is the call stack?
+# The whole reason we are talking about stacks in the first place is so we can understand the call stack. Call stacks help us understand recursion.
+
+# Whenever your program calls a function, the computer sets aside a chunk of memory for its execution context. 
+# Any variables in that function scope are stored here.
+
+# The computer stores these chunks of memory in the call stack, which has two fundamental operations: pushing onto the top and popping off the stack's top. 
+# The computer pushes a chunk on the stack when a function is called. 
+# The computer pops a piece off when a function finishes executing and returns. 
+
+# We have two functions defined in our scope — one to add 2 to our input, and another to add 4.
+
+def add_two(num):
+    return num + 2
+
+def add_four(num):
+    return add_two(add_two(num))
+
+# Let's think about what the call stack looks like when we call these functions. First, what happens when we run the following:
+
+add_two(2)
+add_four(6)
+
+# add_two call gets pushed onto the call stack
+# num gets stored in memory with the value 2
+# num + 2 gets returned as 4 and that call gets popped off
+
+# add_four call gets pushed on the stack, and 6 gets stored as num
+# the innermost add_two call gets pushed onto the stack, and 6 gets stored as num
+# the innermost add_two call returns 8 and pops off
+# the second add_two call gets pushed onto the stack with 8 as num
+# the second add_two call
+# returns 10 and gets popped off
+# we are back in the context of add_four, which now returns 10 and pops off the stack
+# the stack is now empty
+
+
+
+# Next, we look at the Fibonacci Sequence. This is a series of numbers:
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+# We can derive the next number in the sequence by summing the previous two numbers — so the following number in the series shown above would be 55 (34 + 21).
+
+
+# So we can write the 7th term as xn, and you can describe any term in the sequence as:
+
+# x_n = x_{n-1} + x_{n-2}
+
+# Using this rule, if you wanted to find the 10th term, you could write:
+
+# x_{10} = x_{10-1} + x_{10-2}
+# x_{10} = x_9 + x_8
+# x_{10} = 34 + 21
+# x_{10} = 55
+
+def recursive_fib(n):
+    if n <= 1:
+        return n
+    else:
+        return recursive_fib(n-1) + recursive_fib(n-2)
+
+# The base cases are the first two terms in the sequence; the 0th term is 0, and the 1st term is 1. 
+# For every other term, we find the value by summing the previous two terms.
+
+# To more clearly illustrate the building of the stack, we will split our return statement up like this:
+
+def recursive_fib2(n):
+    if n <= 1:
+        return n
+    else:
+        n_minus_1 = recursive_fib2(n-1)
+        n_minus_2 = recursive_fib2(n-2)
+        return n_minus_1 + n_minus_2
+
+# Visualise on Python tutor to understand the call stack
+
+# https://medium.com/popfizzcs/tracing-recursion-6e787c0adc74
+
