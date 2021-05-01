c5722e5df76f825baa4a0e91c1769ec8731c901e
sean-attewell <sean.attewell@gmail.com>
2021-05-01 13:54:32 +0100

    Depth First Search Implementation

--- Data Structures and Algorithms II/graph_DFS.py
+++ Data Structures and Algorithms II/graph_DFS.py
@@ -64,3 +64,66 @@
 # Once it's done exploring all the neighbors, it marks the vert as black (visited).
 
 # https://brilliant.org/wiki/depth-first-search-dfs/
+
+# Implementing depth first search:
+
+# The depth-first search algorithm on a graph starts at an arbitrary vertex in the graph and explores as far as possible down each branch before backtracking. 
+# So, you start at the starting vertex, mark it as visited, and then move to an adjacent unvisited vertex. 
+# You continue this loop until every reachable vertex is visited.
+
+# Requires a stack rather than a list (FIFO) like BFS
+# So with a recursive implementation you're using the call stack as your data structure essentially.
+
+class Vertex:
+    def __init__(self, value):
+        self.value = value
+        self.connections = {}
+
+    def __str__(self):
+        return str(self.value) + ' connections: ' + str([x.value for x in self.connections])
+
+    def add_connection(self, vert, weight = 0):
+        self.connections[vert] = weight
+
+    def get_connections(self):
+        return self.connections.keys()
+
+    def get_value(self):
+        return self.value
+
+    def get_weight(self, vert):
+        return self.connections[vert]
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+        self.count = 0
+
+    def __contains__(self, vert):
+        return vert in self.vertices
+
+    def __iter__(self):
+        return iter(self.vertices.values())
+
+    def add_vertex(self, value):
+        self.count += 1
+        new_vert = Vertex(value)
+        self.vertices[value] = new_vert
+        return new_vert
+
+    def add_edge(self, v1, v2, weight = 0):
+        if v1 not in self.vertices:
+            self.add_vertex(v1)
+        if v2 not in self.vertices:
+            self.add_vertex(v2)
+        self.vertices[v1].add_connection(self.vertices[v2], weight)
+
+    def get_vertices(self):
+        return self.vertices.keys()
+
+    def depth_first_search(self, vertex, visited = set()):
+        visited.add(vertex)
+        for next_vert in vertex.get_connections():
+            if next_vert not in visited:
+                self.depth_first_search(next_vert, visited)
